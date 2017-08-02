716e6dec24de078f81610e7c30c113d6a99a3692
Carlos <carlos.hawari.2101@gmail.com>
2017-08-02 12:14:24 +0300

    merged

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from model import Base, YourModel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+import smtplib
 # setup
 app = Flask(__name__)
 engine = create_engine('sqlite:///project.db')
@@ -29,7 +29,26 @@ def howcanihelp():
 @app.route('/playthegame')
 def playthegame():
     return render_template('playthegame.html')
-    
+
+@app.route('/game', methods=['GET','POST'])
+def take_med():
+	if request.method == 'GET':
+
+		return render_template(' ')
+
 @app.route('/diebetes')
 def diebetes():
     return render_template('diebetes.html')
+
+#while True:
+
+fromaddr = 'diseases.meet@gmail.com'
+toaddrs  = 'kazakov.nicole@gmail.com'
+msg 	 = 'Take your medicine!'
+username = 'diseases.meet@gmail.com'
+password = 'meetissofuckingawesome'
+server   = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
