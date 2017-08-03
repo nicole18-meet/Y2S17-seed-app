6b3cf757b0b21b437aed585a58a7ca3deafb5d84
Carlos <carlos.hawari.2101@gmail.com>
2017-08-03 11:27:15 +0300

    basic game

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from model import Base, User, Routine, Shoot
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import smtplib
+
 # setup
 app = Flask(__name__)
 engine = create_engine('sqlite:///project.db')
@@ -13,59 +14,74 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+MAGIC_USER_ID = 1
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/diseases')
 def diseases():
     return render_template('diseases.html')
+
 
 @app.route('/howcanihelp')
 def howcanihelp():
     return render_template('howcanihelp.html')
 
+
 @app.route('/playthegame')
 def playthegame():
-    user_id = 1
-    routine= session.query(Routine).filter_by(owner=user_id).first()
+    routine = session.query(Routine).filter_by(owner=MAGIC_USER_ID).first()
     return render_template('playthegame.html', routine=routine)
 
-@app.route('/shoots/<int:shoot_id>', methods=['GET','POST'])
-def edit_shoot(shoot_id):
-	shoot = session.query(Shoot).filter_by(id= shoot_id).first()
-	'''
-	if request.method == 'GET':
-		return render_template('playthegame.html',)
-	else:
-		shoots = request.form.get('take_meds')
-		return redirect(url('index.html'))
-	'''
-	if request.form.get('take_meds') == "Took the meds":
-		shoot.is_taken = True
 
+@app.route('/shoots/<int:shoot_id>', methods=['POST'])
+def edit_shoot(shoot_id):
+    shoot = session.query(Shoot).filter_by(id= shoot_id).first()
+    shoot.is_taken = True
+    session.commit()
+    return redirect(url_for('start_game'))
 
 
 @app.route('/game', methods=['GET','POST'])
-def take_med():
-    return render_template('take_med.html')
-
+def start_game():
+    routine = session.query(Routine).first()
+    if not routine:
+    	routine = create_routine() 
+    return render_template('take_meds.html', routine=routine)
 
 
 @app.route('/diebetes')
 def diebetes():
     return render_template('diebetes.html')
 
+
+############### HELPERS ################
+
+def create_routine():
+	r       = Routine(owner=MAGIC_USER_ID)
+	shoot_1 = Shoot(taken_time=datetime('5pm'))
+	shoot_2 = Shoot(taken_time=datetime('4pm'))
+	r.shoots.append(shoot_1)
+	r.shoots.append(shoot_2)
+	session.add(shoot_1)
+	session.add(shoot_2)
+	session.add(r)
+	session.commit()
+	return r
+
 #while True:
 
-fromaddr = 'diseases.meet@gmail.com'
-toaddrs  = 'kazakov.nicole@gmail.com'
-msg 	 = 'Take your medicine!'
-username = 'diseases.meet@gmail.com'
-password = 'meetissofuckingawesome'
-server   = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+# fromaddr = 'diseases.meet@gmail.com'
+# toaddrs  = 'kazakov.nicole@gmail.com'
+# msg 	 = 'Take your medicine!'
+# username = 'diseases.meet@gmail.com'
+# password = 'meetissofuckingawesome'
+# server   = smtplib.SMTP('smtp.gmail.com:587')
+# server.starttls()
+# server.login(username,password)
+# server.sendmail(fromaddr, toaddrs, msg)
+# server.quit()
--- model.py
+++ model.py
@@ -6,20 +6,22 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__  = 'user'
-    id             = Column(Integer, primary_key=True)
-    username       = Column(String)
+    __tablename__ = 'user'
+    id            = Column(Integer, primary_key=True)
+    username      = Column(String)
 
 
 class Routine(Base):
-    __tablename__  = 'routine'
-    id             = Column(Integer, primary_key=True)
-    owner     	   = Column(Integer, ForeignKey("user.id"))
-    shoots         = relationship('Shoot')
+    __tablename__ = 'routine'
+    id            = Column(Integer, primary_key=True)
+    owner         = Column(Integer, ForeignKey("user.id"))
+    shoots        = relationship('Shoot')
     
 
 class Shoot(Base):
-    __tablename__  = 'shoot'
-    id             = Column(Integer, primary_key=True)
-    is_taken       = Column(Boolean, default=False)
-    routine   	   = Column(Integer, ForeignKey("routine.id"))
+    __tablename__ = 'shoot'
+    id            = Column(Integer, primary_key=True)
+    taken_time    = Column(DateTime)
+    is_taken      = Column(Boolean, default=False)
+    is_missed     = Column(Boolean)
+    routine   	  = Column(Integer, ForeignKey("routine.id"))
