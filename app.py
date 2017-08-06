d24436df8d19b74f483fd4b324e372d195b91937
Carlos <carlos.hawari.2101@gmail.com>
2017-08-06 10:49:01 +0300

    database and game fixing

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import datetime
+import sys
 
+import datetime
+from datetime import timedelta
 # flask imports
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -40,8 +42,6 @@ def playthegame():
     routine = session.query(Routine).filter_by(owner=MAGIC_USER_ID).first()
     return render_template('playthegame.html', routine=routine)
 
-from datetime import timedelta
-import sys
 
 @app.route('/shoots/<int:shoot_id>', methods=['POST'])
 def edit_shoot(shoot_id):
@@ -49,12 +49,13 @@ def edit_shoot(shoot_id):
     shoot = session.query(Shoot).filter_by(id= shoot_id).first()
     
     delta = today - shoot.taken_time
-    print("The delta %s" % delta.seconds, file=sys.stderr)
-    if (delta.seconds > 300) or (delta.seconds < 0):
+    print("The delta %s" % delta.total_seconds(), file=sys.stderr)
+    if delta.total_seconds() > 300:
         print("inside missed", file=sys.stderr)
         shoot.is_missed = True
+    elif delta.total_seconds() < 0:
+    	redirect(url_for('start_game'))
     else:
-        print("inside taken", file=sys.stderr)
         shoot.is_taken = True
     session.commit()
     return redirect(url_for('start_game'))
@@ -79,26 +80,53 @@ def diebetes():
 def create_routine():
 	t = datetime.datetime.now()
 	r       = Routine(owner=MAGIC_USER_ID)
-	shoot_1  = Shoot(title="", taken_time=datetime.datetime(t.year, t.month, t.day, 8, 0))
-	shoot_2  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_3  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 12, 22))
-	shoot_4  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_5  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_6  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_7  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_8  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_9  = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_10 = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_11 = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_12 = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_13 = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	shoot_14 = Shoot(taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
-	r.shoots.append(shoot_1)
-	r.shoots.append(shoot_2)
-	r.shoots.append(shoot_3)
-	session.add(shoot_1)
-	session.add(shoot_2)
-	session.add(shoot_3)
+
+	title = {
+		8: "Morning insulin shot + test blood sugar level!",
+		9: "Back from morning jog - sugar level too low! Eat some carbohydrates!",
+		10: "Insulin shot for breakfast + test blood suagr level!",
+		11: "Test blood sugar level!",
+		12: "Test blood sugar level!",
+		13: "Noon insulin shot + test blood sugar level!",
+		14: "Insulin shot for lunch + test blood sugar level!",
+		15: "Sugar level is too high after lunch! take some insulin!",
+		16: "Test blood sugar level!",
+		17: "Test blood sugar level!",
+		18: "Evening insulin shot + test blood sugar level!",
+		19: "Test blood sugar level!",
+		20: "Insulin shot for lunch + test blood sugar level!",
+		21: "Test blood sugar level!",
+		22: "Night insulin shot + test blood sugar level - good night!",
+
+	}
+
+	for i in range(8, 23):
+		taken_time = datetime.datetime(t.year, t.month, t.day, i, 0)
+		shoot = Shoot(
+			title=title.get(i), 
+			taken_time=taken_time
+			)
+		too_late= (t - taken_time).total_seconds()
+		if too_late > 300:
+			shoot.is_missed = True
+		r.shoots.append(shoot)
+		session.add(shoot)
+
+	# shoot_2  = Shoot(title= ,taken_time=datetime.datetime(t.year, t.month, t.day, 9, 0))
+	# shoot_3  = Shoot(title= "Insulin shot for breakfast + test blood suagr level!",taken_time=datetime.datetime(t.year, t.month, t.day, 10, 0))
+	# shoot_4  = Shoot(title="Test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 11, 0))
+	# shoot_5  = Shoot(title="Test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 12, 0))
+	# shoot_6  = Shoot(title="Noon insulin shot + test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 13, 0))
+	# shoot_7  = Shoot(title="Insulin shot for lunch + test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 14, 0))
+	# shoot_8  = Shoot(title="Sugar level is too high after lunch! take some insulin!",taken_time=datetime.datetime(t.year, t.month, t.day, 15, 0))
+	# shoot_9  = Shoot(title="Test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 16, 0))
+	# shoot_10 = Shoot(title="Test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 17, 0))
+	# shoot_11 = Shoot(title="Evening insulin shot + test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 18, 0))
+	# shoot_12 = Shoot(title="Test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 19, 0))
+	# shoot_13 = Shoot(title="Insulin shot for lunch + test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 20, 0))
+	# shoot_14 = Shoot(title="Test blood sugar level!",taken_time=datetime.datetime(t.year, t.month, t.day, 21, 0))
+	# shoot_15 = Shoot(title="Night insulin shot + test blood sugar level - good night!",taken_time=datetime.datetime(t.year, t.month, t.day, 22, 0))
+
 	session.add(r)
 	session.commit()
 	return r
