f2333432731ee9c2cee23dd24c442688a8730075
nicole18-meet <nicole18-meet>
2017-08-06 10:48:03 +0300

    merge solved

--- static/js/game.js
+++ static/js/game.js
@@ -1,14 +1,14 @@
-function hour_8(){
-	var d = new Date();
-	var hour = d.getHours();	
-	var min = d.getMinutes();
-	if ((hour === 8) && ((min > 0 ) && (min < 5))){
-    	window.alert("Medicine registered!");
-		}
-	else{
-		window.alert("It's too late!");
-		}
-	}	
+////function hour_8(){
+	//var d 	 = new Date();
+	//var hour = d.getHours();	
+	//var min  = d.getMinutes();
+	//if ((hour === 8) && ((min > 0 ) && (min < 5))){
+   // 	window.alert("Medicine registered!");
+	//	}
+	//else{
+	//	window.alert("It's too late!");
+	//}
+//}	
 
 // - send email with python https://stackoverflow.com/questions/10147455/how-to-send-an-email-with-gmail-as-provider-using-python
 // - add button to page that trigger the email to e sent after
